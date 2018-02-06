29958366d90def8d28517a9dbe9c85cee2bcd56e
devanshisukhija <tech.devanshi@gmail.com>
2018-02-06 21:30:58 +0530

    nav and main before login page

--- app/src/main/java/com/devanshisukhija/smack/MainActivity.kt
+++ app/src/main/java/com/devanshisukhija/smack/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -29,4 +30,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun loginNavBtnClicked(view:View) {
+
+    }
+
+    fun addChannelClicked(view: View) {
+
+    }
+
+    fun senMsgBtnClicked(view:View) {
+
+    }
 }
