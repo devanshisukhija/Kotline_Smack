6aafca8577763fb6be06afc1818fd42a1e1c4768
devanshisukhija <tech.devanshi@gmail.com>
2018-02-07 13:38:29 +0530

    login activity and added to nav header Btn

--- app/src/main/java/com/devanshisukhija/smack/MainActivity.kt
+++ app/src/main/java/com/devanshisukhija/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.devanshisukhija.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
@@ -31,6 +32,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginNavBtnClicked(view:View) {
+
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
 
     }
 
